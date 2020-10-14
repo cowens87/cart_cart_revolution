22e3659142ecb380236108b491e1adc413e32c87
Cydnee Owens <cks410@yahoo.com>
2020-10-14 13:25:30 -0600

    Add products method

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -17,11 +17,11 @@ class ShoppingCartTest < Minitest::Test
   end
 
   def test_it_can_add_products
-    assert_equal [], cart.products
+    assert_equal [], @cart.products
 
-    cart.add_product(product1)
-    cart.add_product(product2)
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
 
-    assert_equal [product1, product2], cart.products
+    assert_equal [@product1, @product2], @cart.products
   end
 end
